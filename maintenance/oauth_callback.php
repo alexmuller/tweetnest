c6227393eb696c40b42770843d58c17588c2abe8
Alex Muller <alexlmuller@gmail.com>
2012-08-20 12:16:29 +0100

    Store OAuth credentials in the DB

--- maintenance/oauth_callback.php
+++ maintenance/oauth_callback.php
@@ -1,35 +1,37 @@
 <?php
 
-  session_start();
-  require "mpreheader.php";
-  $pageTitle = "Authenticated with Twitter!";
-  require "mheader.php";
-  require_once "../lib/twitteroauth.php";
+	session_start();
+	require "mpreheader.php";
+	$pageTitle = "Authenticated with Twitter!";
+	require "mheader.php";
+	require_once "../lib/twitteroauth.php";
 
-  // If the oauth_token is old redirect to the connect page.
-  if (isset($_REQUEST['oauth_token']) && $_SESSION['oauth_token'] !== $_REQUEST['oauth_token']) {
-    session_destroy();
-    header('Location: ./oauth_setup.php');
-  }
+	// If the oauth_token is old redirect to the connect page.
+	if (isset($_REQUEST['oauth_token']) && $_SESSION['oauth_token'] !== $_REQUEST['oauth_token']) {
+		session_destroy();
+		header('Location: ./oauth_setup.php');
+	}
 
-  // Create TwitteroAuth object with app key/secret and token key/secret from default phase
-  $connection = new TwitterOAuth($config['consumer_key'], $config['consumer_secret'], $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
+	// Create TwitteroAuth object with app key/secret and token key/secret from default phase
+	$connection = new TwitterOAuth($config['consumer_key'], $config['consumer_secret'], $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
 
-  // Request access tokens from Twitter
-  $access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
+	// Request access tokens from Twitter
+	$access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
 
-  // Remove no longer needed request tokens
-  unset($_SESSION['oauth_token']);
-  unset($_SESSION['oauth_token_secret']);
+	// Remove no longer needed request tokens
+	unset($_SESSION['oauth_token']);
+	unset($_SESSION['oauth_token_secret']);
 
-  // If HTTP response is 200 continue otherwise send to connect page to retry
-  if (200 == $connection->http_code) {
-    // The user has been verified and the access tokens can be saved for future use
-    echo l("<p>All done! We need to save this in the DB:</p>");
-    var_dump($access_token);
-  } else {
-    // Save HTTP status for error dialog on connnect page.
-    header('Location: ./oauth_setup.php');
-  }
+	// If HTTP response is 200 continue otherwise send to connect page to retry
+	if (200 == $connection->http_code) {
+		// The user has been verified and the access tokens can be saved for future use
+		echo l("Authenticated fine with Twitter. Saving your tokens in the database now...\n");
+		$iq = "UPDATE `".DTP."tweetusers` SET `oauth_token` = '" . $access_token['oauth_token'] . "', `oauth_token_secret` = '" . $access_token['oauth_token_secret'] . "' WHERE `userid` = '" . $access_token['user_id'] . "' LIMIT 1";
+		$q = $db->query($iq);
+		echo $q ? l(good("All done!")) : l(bad("DATABASE ERROR: " . $db->error()));
+	} else {
+		// Save HTTP status for error dialog on connnect page.
+		header('Location: ./oauth_setup.php');
+	}
 
-  require "mfooter.php";
+	require "mfooter.php";
--- maintenance/oauth_redirect.php
+++ maintenance/oauth_redirect.php
@@ -1,35 +1,31 @@
 <?php
 
-  session_start();
-  require "mpreheader.php";
-  $pageTitle = "Redirecting you to Twitter to authenticate...";
-  require "mheader.php";
-  require_once "../lib/twitteroauth.php";
+	session_start();
+	require "mpreheader.php";
+	$pageTitle = "Redirecting you to Twitter to authenticate...";
+	require "mheader.php";
+	require_once "../lib/twitteroauth.php";
 
-  // Build TwitterOAuth object with client credentials.
-  $connection = new TwitterOAuth($config['consumer_key'], $config['consumer_secret']);
+	// Build TwitterOAuth object with client credentials.
+	$connection = new TwitterOAuth($config['consumer_key'], $config['consumer_secret']);
 
-  // Get temporary credentials.
-  $callback_path = $config['twitter_callback'] + "maintenance/oauth_callback.php";
-  $request_token = $connection->getRequestToken("http://localhost:80/maintenance/oauth_callback.php");
+	// Get temporary credentials.
+	$callback_path = $config['twitter_callback'] + "maintenance/oauth_callback.php";
+	$request_token = $connection->getRequestToken("http://localhost:80/maintenance/oauth_callback.php");
 
-  // Save temporary credentials to session.
-  $_SESSION['oauth_token'] = $token = $request_token['oauth_token'];
-  $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+	// Save temporary credentials to session.
+	$_SESSION['oauth_token'] = $token = $request_token['oauth_token'];
+	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
 
-  switch ($connection->http_code) {
-    case 200:
-      // Build authorize URL and redirect user to Twitter.
-      $url = $connection->getAuthorizeURL($token);
-      header('Location: ' . $url); 
-      break;
-    default:
-      // Show notification if something went wrong.
-      echo 'Could not connect to Twitter. Refresh the page or try again later.';
-      echo '<pre>';
-      echo $connection->http_code;
-      echo print_r($connection->http_info);
-      echo '</pre>';
-  }
+	switch ($connection->http_code) {
+		case 200:
+			// Build authorize URL and redirect user to Twitter.
+			$url = $connection->getAuthorizeURL($token);
+			header('Location: ' . $url); 
+			break;
+		default:
+			// Show notification if something went wrong.
+			echo 'Could not connect to Twitter. Refresh the page or try again later.';
+	}
 
-  require "mfooter.php";
+	require "mfooter.php";
