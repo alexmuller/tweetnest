a5205fe4dd4507eab5f419be2ddb6b78683b089d
Alex Muller <alexlmuller@gmail.com>
2012-08-22 15:12:31 +0100

    More clear instructions after migrating the DB

--- upgrade/upgrade.20120820.php
+++ upgrade/upgrade.20120820.php
@@ -6,6 +6,7 @@
 
 	$db->query("ALTER TABLE `".DTP."tweetusers` ADD COLUMN oauth_token varchar(50), ADD COLUMN oauth_token_secret varchar(50)") or die($db->error());
 	echo "Added oauth_token and oauth_token_secret columns to the users table.\n";
-	echo "Visit ./maintenance/oauth_redirect.php to load your credentials into the database.\n";
+	echo "First, make sure your Twitter application credentials are stored in /inc/config.php\n";
+	echo "Then visit /maintenance/oauth_redirect.php to load your credentials into the database.\n";
 
 	echo "Done! You can delete me now.\n";
